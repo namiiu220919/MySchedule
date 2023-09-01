cff1fd2a34cc0779d061288259bad4437c22dbe8
nguyenthuc04 <130113818+nguyenthuc04@users.noreply.github.com>
2023-09-01 23:42:01 +0700

    thuc

--- app/src/main/java/com/example/myschedule/fragment/Fragmenthome.java
+++ app/src/main/java/com/example/myschedule/fragment/Fragmenthome.java
@@ -6,22 +6,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
 
 import com.example.myschedule.R;
-import com.example.myschedule.adapter.danhsach_home_adapter;
-import com.example.myschedule.dao.dsdao;
 import com.example.myschedule.databinding.FragmentFragmenthomeBinding;
 import com.example.myschedule.model.danhsachthoigian;
 
 import java.util.ArrayList;
 
 public class Fragmenthome extends Fragment {
-private FragmentFragmenthomeBinding binding;
-private danhsach_home_adapter adapter;
-    dsdao dsDao;
-private ArrayList<danhsachthoigian> list = new ArrayList<>();
 
     public Fragmenthome() {
         // Required empty public constructor
@@ -31,12 +24,6 @@ private ArrayList<danhsachthoigian> list = new ArrayList<>();
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.fragment_fragmenthome, container, false);
-//        LinearLayoutManager layout = new LinearLayoutManager(getActivity());
-//        dsDao = new dsdao(getActivity());
-//        list=dsDao.selectAll();
-//        binding.Recycleviewdanhsach.setLayoutManager(layout);
-//        adapter = new danhsach_home_adapter(getActivity(),list);
-//        binding.Recycleviewdanhsach.setAdapter(adapter);
         return view ;
     }
 }
